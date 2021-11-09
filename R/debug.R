6b0030cd1d168c4f4ce0a963cfb81757b3b20175
Pete Laud <p.j.laud@shef.ac.uk>
2021-11-09 12:18:07 +0000

    Debugging

--- R/debug.R
+++ R/debug.R
@@ -1,4 +1,5 @@
 install.packages('ratesci')
 library(ratesci)
 
-scaspci(x = 1, n = 9333, level = 0.95)
+myout <- scaspci(x = 1:3, n = 9333, level = 0.999)
+save(myout, file = paste0(".github/workflows/data_", make.names(Sys.time()), ".Rda"))
