9a72d4167195773b7e2b7a1b58efc355efd67719
Pete Laud <p.j.laud@shef.ac.uk>
2021-11-27 17:26:44 +0000

    Remove Rmpfr

--- R/rateci.R
+++ R/rateci.R
@@ -48,8 +48,8 @@ scaspci <- function(x,
                     level = 0.95,
                     cc = FALSE,
                     ...) {
-  x <- Rmpfr::mpfr(x, 120)
-  n <- Rmpfr::mpfr(n, 120)
+#  x <- Rmpfr::mpfr(x, 120)
+#  n <- Rmpfr::mpfr(n, 120)
   if (as.character(cc) == "TRUE") cc <- 0.5
   z <- qnorm(1 - (1 - level) / 2)
   if (distrib == "poi") {
