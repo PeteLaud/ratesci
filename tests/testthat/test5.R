7b9dc0421089bb4e902b154b919fa0a1dc0f8b03
Pete Laud <p.j.laud@shef.ac.uk>
2021-11-27 23:17:25 +0000

    Tweak test5

--- tests/testthat/test5.R
+++ tests/testthat/test5.R
@@ -7,12 +7,10 @@ context("Consistency")
 # conditions.
 # Seems to be OK for level≤0.99
 
-n <- 9333
-xs <- 0:9333
+n <- 8000
+xs <- 0:8000
 rounded <- 10
-#level <- 0.9
 for (level in c(0.9, 0.95, 0.99, 0.999)) {
-#for (level in c(0.9)) {
   test_that("noniterative scas matches iterative version", {
     expect_equal(
       round(scoreci(x1 = xs, n1 = n, contrast = "p", level = level, precis = rounded + 1)$estimates[, c(1:3)], rounded),
@@ -20,7 +18,7 @@ for (level in c(0.9, 0.95, 0.99, 0.999)) {
     )
     expect_equal(
       round(scoreci(x1 = xs, n1 = n, contrast = "p", cc = T, level = level, precis = rounded + 1)$estimates[, c(1:3)], rounded),
-      round(scaspci(x = xs, n = n, cc = T, level = level)[, c(1:3)], rounded) #Or is it this one?
+      round(scaspci(x = xs, n = n, cc = T, level = level)[, c(1:3)], rounded) #Or is it this one? Fixed by removing Rmpfr code
     )
     expect_equal(
       round(scoreci(x1 = xs, n1 = n, contrast = "p", level = level, distrib = "poi", precis = rounded + 1)$estimates[, c(1:3)], rounded),
